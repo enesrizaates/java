b39c33ed8d3bdf205c48f1905bda526f7297cba9
enesrizaates <enesrates@gmail.com>
2024-07-09 19:33:00 +0300

    keyboard input practice

--- Section2-Project/src/KeyboardInput.java
+++ Section2-Project/src/KeyboardInput.java
@@ -7,19 +7,29 @@ public class KeyboardInput {
         String name;
         String city;
         int age;
+        double number;
 
         System.out.println("What is your name?: ");
         name = keyboard.nextLine();
 
         System.out.println("What is your age?: ");
         age = keyboard.nextInt();
+
+
+        System.out.println("What is your number?: ");
+        number = keyboard.nextDouble();
+        number *= 2;
+
         keyboard.nextLine(); // consume the new line
 
         System.out.println("What is your city?: ");
         city= keyboard.nextLine();
 
+
+
         System.out.println("Hello, " + name);
         System.out.println("Your age is " + age);
         System.out.println("Your city is " + city);
+        System.out.println("Twice your number is " + number);
     }// end main
 }
